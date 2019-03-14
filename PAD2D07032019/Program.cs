b98ad802759da344a92781aea29e0364a0779919
tucoff <baracho.arturraffael@gmail.com>
2019-03-14 14:53:54 -0300

    Primeira versão

---
+++ PAD2D07032019/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PAD2D07032019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("BOM DIA BAIANOR       ");                        
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("            :D I ");
+            Console.WriteLine("         c---I-- ");
+            Console.WriteLine("             I    ");
+            Console.WriteLine("            I   ");
+            Console.WriteLine("           P-R     ");
+            Console.WriteLine("          I   I    ");
+            Console.WriteLine("         I   I   ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("'''''''''''''''''''''''''''");
+            Thread.Sleep(400);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("BOM DIA BAIANOR       ");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("            :D  I ");
+            Console.WriteLine("            -I-- ");
+            Console.WriteLine("         c-' I    ");
+            Console.WriteLine("             I   ");
+            Console.WriteLine("            P-R     ");
+            Console.WriteLine("            I  I    ");
+            Console.WriteLine("             II   ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("'''''''''''''''''''''''''''");
+            Thread.Sleep(400);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("");
+            Console.WriteLine("BOM DIA BAIANOR       ");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("            ;D I");
+            Console.WriteLine("         c---I-- ");
+            Console.WriteLine("             I   ");
+            Console.WriteLine("            I   ");
+            Console.WriteLine("           P-R     ");
+            Console.WriteLine("          I   I    ");
+            Console.WriteLine("         I   I   ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("'''''''''''''''''''''''''''");
+            Thread.Sleep(400);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("BOM DIA BAIANOR      ");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("            ;D  I");
+            Console.WriteLine("            -I-- ");
+            Console.WriteLine("         c-' I   ");
+            Console.WriteLine("             I   ");
+            Console.WriteLine("            P-R     ");
+            Console.WriteLine("            I  I    ");
+            Console.WriteLine("             II   ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("'''''''''''''''''''''''''''");
+            Thread.Sleep(400);
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.Clear();
+            Main(args);
+        }
+    }
+}
